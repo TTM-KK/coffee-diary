6a4022344896902c66182340f797c6f00377b64d
koki <kkttm530@gmail.com>
2020-08-30 21:35:36 +0900

    first commmit

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="header__box">
+      <nuxt-link to="/" class="header__text">COFFEE DIARY</nuxt-link>
+    </div>
+    <Nuxt />
+  </div>
+</template>
+
+<style scoped>
+.header__box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #cbb77d;
+  height: 30px;
+}
+
+.header__text {
+  text-decoration: none;
+  color: #000;
+}
+
+.header__link {
+  color: #000;
+}
+</style>
---
+++ pages/pourover.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <img src="@/assets/svg/coffee_drip.svg" style="width: 100%;" />
+  </div>
+</template>
